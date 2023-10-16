4d4550bd802482145c97eb0ec9b7e6ed41bdb4ed
Fernando Nunes <nunesfernandopro@gmail.com>
2023-10-16 12:43:59 -0300

    First commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn projects.wsgi
+web: gunicorn crud_python.wsgi
--- crud_python/settings.py
+++ crud_python/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku.settings(locals)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
